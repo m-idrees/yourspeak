ba333c7788ef03db261c69a454c36d890a7baca8
bob <bob@>
2025-05-14 18:23:50 +0400

    gitignore updated

--- cypress/e2e/specs/login-logout.cy.js
+++ cypress/e2e/specs/login-logout.cy.js
@@ -9,6 +9,7 @@ describe('template spec', () => {
     Cypress.on('uncaught:exception', (err, runnable) => {
       // returning false here prevents Cypress from
       // failing the test
+      //
       return false
     })
   })
